e980f349c7e2adbf0673a8f02cc54c03a538cbf0
euseanwoon2016 <20278298+euseanwoon2016@users.noreply.github.com>
2021-11-19 09:59:41 +0800

    :apple: test commit 3

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const sequelize = new Sequelize({
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  client.user.setActivity('with guild reports.', { type: 'PLAYING' });
+  client.user.setActivity('with guild documents.', { type: 'PLAYING' });
 });
 
 client.on('messageCreate', (message) => {
